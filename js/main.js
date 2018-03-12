1633fcf2d118799be0308fd92dccd5c5b7ddfcef
DimiG <dim.georgiev1976@yandex.com>
2018-03-12 20:21:51 +0800

    calculator-solar

--- js/main.js
+++ js/main.js
@@ -1,72 +1,109 @@
 $(document).ready(function(){
 
+				// jQuery - Авто калькулятор
+
+	var modelSpecs,
+			modelPrice,
+			modelSpecsHolder,
+			modelPriceHolder;
+			// modelPriceUSDHolder;
+
+
+				modelSpecsHolder = $('#modelSpecs'),
+				modelPriceHolder = $('#modelPrice'),
+				modelPriceUSDHolder = $('#modelPriceUSD');
+
+			
+			modelSpecs = '';
+			modelPrice = 0;
+
+						// При старте страницы
+	calculatePrice();
+	compileSpecs();
+
+	
+	// ВЫБОР ЦВЕТА - на цену не влияет
 	$('#colorsSelector .colorItem').on('click', function(){
 		var imgPath = $(this).attr('data-img-path');
 		$('#imgHolder img').attr('src', imgPath);
 	});
 
-	var modelSpecs = '',
-		modelPrice = 0,
-		modelSpecsHolder = $('#modelSpecs'),
-		modelPriceHolder = $('#modelPrice'),
-		modelPriceUSDHolder = $('#modelPriceUSD');
 
-	function calculatePrice(){
-		modelPrice = parseInt($('input[name=engine]:checked', '#autoForm').val());
-		modelPrice += parseInt($('input[name=transmission]:checked', '#autoForm').val());
-		modelPrice += parseInt($('input[name=package]:checked', '#autoForm').val());
-		// alert(modelPrice);
-		modelPriceHolder.text( addSpace(modelPrice) + ' рублей');
-	};
+		   //price calculator
+    function calculatePrice(){
+        var modelPriceEngine = $('input[name=engine]:checked', '#autoForm').val();
+        var modelPriceTransmission = $('input[name=transmission]:checked', '#autoForm').val();
+        var modelPricePackage = $('input[name=package]:checked', '#autoForm').val();
+        
+        modelPriceEngine = parseInt(modelPriceEngine);
+        modelPriceTransmission = parseInt(modelPriceTransmission);
+        modelPricePackage = parseInt(modelPricePackage);
 
-	function compileSpecs(){
+        modelPrice = modelPriceEngine + modelPriceTransmission + modelPricePackage;
+
+        modelPriceHolder.text(addSpace(modelPrice) + ' рублей');
+    };
+
+// После переключения радио кнопок
+
+	$('#autoForm input').on('change', function(){
+	
+		compileSpecs();
+		calculateUSD();
+		calculatePrice();
+	});
+
+
+
+		function compileSpecs(){
 		modelSpecs = $('input[name=engine]:checked + label', '#autoForm').text();
-		modelSpecs += ', ' + $('input[name=transmission]:checked + label', '#autoForm').text();
-		modelSpecs += ', ' + $('input[name=package]:checked + label', '#autoForm').text() + '.';
-		// alert(modelSpecs);
+		modelSpecs = modelSpecs + ', ' + $('input[name=transmission]:checked + label', '#autoForm').text();
+		modelSpecs = modelSpecs + ', ' + $('input[name=package]:checked + label', '#autoForm').text() + '.';
+	
 		modelSpecsHolder.text(modelSpecs);
 	};
 
-	$('#autoForm input').on('change', function(){
-		calculatePrice();
-		compileSpecs();
-		//calculateUSD();
-	});
-
-	calculatePrice();
-	compileSpecs();
-
-
 	function addSpace(nStr) {
-	    nStr += '';
-	    x = nStr.split('.');
-	    x1 = x[0];
-	    x2 = x.length > 1 ? '.' + x[1] : '';
-	    var rgx = /(\d+)(\d{3})/;
-	    while (rgx.test(x1)) {
-	        x1 = x1.replace(rgx, '$1' + ' ' + '$2');
-	    }
-	    return x1 + x2;
-	}
+					nStr += '';
+					x = nStr.split('.');
+					x1 = x[0];
+					x2 = x.length > 1 ? '.' + x[1] : '';
+					var rgx = /(\d+)(\d{3})/;
+					while (rgx.test(x1)) {
+					x1 = x1.replace(rgx, '$1' + ' ' + '$2');
+					}
+					return x1 + x2;
+					}
+									//Получаем курс валют
+								var currencyUrl = 'https://www.cbr-xml-daily.ru/daily_json.js';
+								var rurUsdRate = 0;
 
-	// Получаем курс валют
-	var currencyUrl = 'https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+"USDRUB,EURRUB"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-	var rurUsdRate = 0;
+								$.ajax({
+								url: currencyUrl,
+								cache: false,
+								success: function(data){
 
-	$.ajax({
-		url: currencyUrl,
-		cache: false,
-		success: function(html){
-			console.log(html.query);
-			//rurUsdRate = html.query.results.rate[0].Rate;
-			//calculateUSD();
-		} 
-	});
+								data = JSON.parse(data);
 
-	function calculateUSD(){
-		var modelPriceUSD = modelPrice / rurUsdRate;
-		// alert(modelPriceUSD);
-		modelPriceUSDHolder.text( '$ ' + addSpace( modelPriceUSD.toFixed(0) ) );
+								console.log(data.Valute.USD.Value);
+
+								rurUsdRate = data.Valute.USD.Value;
+
+								calculateUSD();
+
+								}
+								});
+
+
+				function calculateUSD(){
+			var modelPriceUSD = modelPrice / rurUsdRate;
+			// alert(modelPriceUSD);
+			modelPriceUSDHolder.text( '$ ' + addSpace( modelPriceUSD.toFixed(0) ) );
 	}
 
 });
+
+
+
+
+
